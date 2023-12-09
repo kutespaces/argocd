ca4644c16db8b62988376bc2e8552c8b4ec43d27
Nico Duldhardt <36307788+2start@users.noreply.github.com>
2023-12-09 12:16:19 +0000

    wait until argocd is ready before deploy app of apps

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -45,6 +45,16 @@ main() {
   kubectl patch secret -n argocd argocd-secret -p '{"stringData": {"admin.password": "$2y$10$49UjmQBCYm406qFoLdyzhO72DKJ9JM7m3uAO70vvapSseAPf2ZTcy"}}'
   kubectl apply -k manifests/git-repo-server
   # deploy app of apps which deploys all other apps
+
+    
+
+  ELAPSED_TIME=0
+  # Wait for the CRD to be installed with a timeout
+  until kubectl get crd applications.argoproj.io &> /dev/null || [ $ELAPSED_TIME -ge 60 ]; do
+    echo "Waiting for Argo CD Application CRD to be installed..."
+    sleep 10
+    ELAPSED_TIME=$((ELAPSED_TIME+10))
+  done
   kubectl apply -f manifests/app-of-apps.yaml
 
   echo "on-create end"
