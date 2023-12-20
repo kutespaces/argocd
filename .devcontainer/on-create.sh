0aa573c643cd726151bb3e9a69e51241e8a0e28b
Nico Duldhardt <36307788+2start@users.noreply.github.com>
2023-12-20 07:58:31 +0000

    incr. git repo server timeout

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -82,7 +82,7 @@ main() {
     ELAPSED_TIME=$((ELAPSED_TIME+5))
   done
   echo "Waiting for argocd repo server to be ready"
-  kubectl wait --namespace argocd --for=condition=Ready pod -l app.kubernetes.io/component=repo-server,app.kubernetes.io/instance=argocd --timeout=60s
+  kubectl wait --namespace argocd --for=condition=Ready pod -l app.kubernetes.io/component=repo-server,app.kubernetes.io/instance=argocd --timeout=90s
   echo "Argocd repo server is ready"
   kubectl apply -f manifests/app-of-apps.yaml
 
